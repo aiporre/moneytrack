27a58a5a05191055c4c3c66cb93ad37157d80c6b
aiporre <ariel.iporre.rivas@gmail.com>
2022-07-05 07:19:35 +0200

    create get invoices and invoice items

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const _ = require('lodash');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const bcrypt = require('bcrypt');
+const { request } = require('express');
 const saltRounds = 10;
 
 const PORT = process.env.PORT || 3128;
@@ -115,7 +116,7 @@ app.post('/login', (req, res) => {
 });
 
 // Post /invoice - add a new purchase to the database with fields: name, user_id, and price
-app.post('/invoice', (req, res) => {
+app.post('/invoice', upload.none(), (req, res) => {
     // check data is not empty
     if (!req.body.name || !req.body.user_id || !req.body.price) {
         // return json with status and message
@@ -168,8 +169,66 @@ app.post('/invoice', (req, res) => {
         });
 
     });
+    db.close();
+});
+
+// GET /invoice/user/{user_id} - get all invoices for a user
+app.get('/invoice/user/:user_id', upload.none(), (req, res) => {
+    // check data is not empty
+    if (!req.params.user_id) {
+        // return json with status and message
+        response = res.json({"status": false, "message": "Please fill in all fields"});
+        return response;
+    }
+    // search all invoice where user_id equal to user_id in url
+    let db = new sqlite3.Database('./database/InvApp.db');
+    let sqlCommand = `SELECT * FROM 
+                        invoices 
+                        WHERE 
+                            user_id = '${req.params.user_id}'
+                        ORDER BY 
+                            invoice_id DESC`;
+    db.all(sqlCommand, [], (err, row) => {
+        if (err) {
+            throw err;
+        } else {
+            if (row.length === 0) {
+                response = res.json({"status": false, "message": "User not found"});
+                return response;
+            } else {
+                response = res.json({"status": true, "message": "Invoices found", "invoices": row});
+                return response;
+            }
+        }
+    });
+});
 
 
+// GET /invoice/user/{user_id}/{invoice_id} - get all items for a user
+app.get('/invoice/user/:user_id/:invoice_id', upload.none(), (req, res) => {
+    // check data is not empty
+    if (!req.params.user_id || !req.params.invoice_id) {
+        // return json with status and message
+        response = res.json({"status": false, "message": "Please fill in all fields"});
+        return response;
+    }
+    // search invoices and transaction with user_id and invoice_id in url
+    let db = new sqlite3.Database('./database/InvApp.db');
+    let sqlCommand = `SELECT * FROM invoices LEFT JOIN transactions
+                        ON invoices.invoice_id = transactions.invoice_id
+                        WHERE invoices.user_id = '${req.params.user_id}'
+                        AND invoices.invoice_id = '${req.params.invoice_id}
+                        ORDER BY transactions.transaction_id ASC`;
+    db.all(sqlCommand, [], (err, row) => {
+        if (err) {
+            throw err;
+        } else {
+            // return row found
+            response = res.json({"status": true, "message": "Invoices found", "transactions": row});
+            return response;
+        }
+    });
+});
 // listen to port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
